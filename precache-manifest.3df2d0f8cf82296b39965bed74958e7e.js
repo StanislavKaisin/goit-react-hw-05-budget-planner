618d814aa238ccb69c4735168fcc8931d1d5e818
Stanislav Kaisin <8050361@gmail.com>
2019-07-28 18:00:35 +0300

    Updates

---
+++ precache-manifest.3df2d0f8cf82296b39965bed74958e7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2425cb492d39715cf45bd82433aaf7",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "4ed768f433e7c0014d06",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "cd8f3818d27f203e7bc9",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.a525ea21.chunk.js"
+  },
+  {
+    "revision": "4ed768f433e7c0014d06",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.f033be7c.chunk.js"
+  },
+  {
+    "revision": "549cb78f4abd9d607011",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.0cda3cff.js"
+  }
+]);
